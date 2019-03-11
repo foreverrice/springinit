544f81c775412cf04fc78ca02cdd17750d4b8f66
liyunwen <liyunwen@corp-ci.com>
2019-03-11 17:52:51 +0800

    走到service层

--- src/main/java/com/jarry/time/Controller/TestMySql.java
+++ src/main/java/com/jarry/time/Controller/TestMySql.java
@@ -3,21 +3,26 @@ package com.jarry.time.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import com.jarry.time.Service.Test;
 @Controller
 @ResponseBody
 @RequestMapping(value = "/mysql")
 
 public class TestMySql {
+    private class TestOne extends com.jarry.time.Service.Test{}
     @RequestMapping(value = "", method = RequestMethod.POST)
     public String insert() {
         return "insert";
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String get() {
-        return "get";
+    public Integer get(@RequestParam(name = "id", defaultValue = "1") int id) {
+        TestOne test = new TestOne();
+        test.get(id);
+        System.out.println(id);
+        return id;
     }
 
     @RequestMapping(value = "search", method = RequestMethod.GET)
---
+++ src/main/java/com/jarry/time/Service/Test.java
@@ -0,0 +1,11 @@
+package com.jarry.time.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Test {
+    public Integer get(int id) {
+
+        return id;
+    }
+}
